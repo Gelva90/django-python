59531db0534b6bff37af94c2c50774cdf617f724
davidkennefick <david.kennefick@gmail.com>
2015-12-08 19:38:13 +0000

    pluck for nmap IP's
    its been nearly a year since i've worked on this

---
+++ pluck.py
@@ -0,0 +1,29 @@
+import xml.dom.minidom
+import sys
+import getopt
+try: 
+    nmapresult = sys.argv[1]
+except:
+    print "XML doc"
+if nmapresult:
+    doc = xml.dom.minidom.parse(nmapresult)
+    output = []
+    for host in doc.getElementsByTagName("host"):
+        ip = ''
+        ports = ''
+        addresses = host.getElementsByTagName("address")
+        ip = addresses[0].getAttribute("addr")
+        #print ip
+        manyports = host.getElementsByTagName("port")
+        #print ports
+        for p in manyports:
+            try:
+                if p.getAttribute("key") == 'portid':
+                    if portid == '':
+                        portid = p.childNodes[0].nodeValue
+            except:
+                pass
+        print p#"".join([str(x) for x in p] )
+        output.append(ip + ',' + ports)
+    for i in output:
+        print i
